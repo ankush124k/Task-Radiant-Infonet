4a958a8b176e55c14c82cc565d524e20b9a00ec8
Ankush kushwaha <83585893+ankush124k@users.noreply.github.com>
2024-06-11 21:51:31 +0530

    Uploading all the files except node_modules folder

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ProductItem from './ProductItem';
+const ProductList = () => {
+  return (
+    <>
+    <ProductItem/>
+    </>
+  )
+}
+
+export default ProductList
